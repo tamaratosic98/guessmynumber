052ce0c1ea079736ae96e7de51a2a8b47c836236
tamaratosic98 <tosict98@gmail.com>
2021-06-18 13:56:47 +0200

    Pristup DOM elementima

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+document.querySelector('.message'); //da je bio id onda bismo stavili #message
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.guess').value = 23;
